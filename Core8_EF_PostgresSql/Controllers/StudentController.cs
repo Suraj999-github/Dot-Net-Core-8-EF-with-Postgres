cc96e7f12f18b3c644f559e8ff4f92d036daaed6
Suraj Goud <68551772+Suraj999-github@users.noreply.github.com>
2024-05-13 10:25:17 +0545

    Entity core 8 with postgres DB

---
+++ Core8_EF_PostgresSql/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using Core8_EF_PostgresSql.DbContexts;
+using Core8_EF_PostgresSql.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core8_EF_PostgresSql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }       
+        [HttpPost]
+        [Route("/add")]
+        public async Task<Object> add(AddStudent req)
+        {
+            try
+            {
+                Student entity = new Student();
+                entity.PhoneNumber = req.PhoneNumber;
+                entity.FirstName = req.FirstName;
+                entity.LastName = req.LastName;
+                entity.MiddleName = req.MiddleName;
+                entity.Salutation = req.Salutation;
+                await _context.student.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                var msg= ex.Message;
+                return "Failed";
+            }
+        }
+        [HttpPut]
+        [Route("/update")]
+        public async Task<Object> update(UpdateStudent req)
+        {
+            try
+            {
+                Student entity = new Student();
+                entity= await _context.student.FindAsync(req.Id);
+                if(entity!=null)
+                {
+                    entity.PhoneNumber = req.PhoneNumber;
+                    entity.FirstName = req.FirstName;
+                    entity.LastName = req.LastName;
+                    entity.MiddleName = req.MiddleName;
+                    entity.Salutation = req.Salutation;
+                    _context.student.Update(entity);
+                    await _context.SaveChangesAsync();
+                }              
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return "Failed";
+            }
+        }
+        [HttpGet]
+        [Route("/get/{id}")]
+        public async Task<Object> getById(long id)
+        {
+            try
+            {
+                return await _context.student.FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        [HttpGet]
+        [Route("/list")]
+        public async Task<Object> list()
+        {
+            try
+            {
+                return await _context.student.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ Core8_EF_PostgresSql/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Core8_EF_PostgresSql.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core8_EF_PostgresSql.DbContexts
+{  
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+       public DbSet<Student> student { get; set; }       
+
+    }
+}
